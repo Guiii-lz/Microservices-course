c944ceaa08305ed8279adb49ef1a2e3558d11529
Guiii-lz <guima.luiz@hotmail.com>
2024-07-23 21:04:28 -0300

    Implementando balanceamento de carga com Ribbon

--- hr-worker/src/main/java/io/github/guiiilz/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/io/github/guiiilz/hrworker/resources/WorkerResource.java
@@ -1,32 +1,40 @@
 package io.github.guiiilz.hrworker.resources;
 
-import java.util.List;
-
+import io.github.guiiilz.hrworker.entities.Worker;
+import io.github.guiiilz.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.guiiilz.hrworker.entities.Worker;
-import io.github.guiiilz.hrworker.repositories.WorkerRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	@Autowired
+	private Environment environment;
 	@Autowired
 	private WorkerRepository workerRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = workerRepository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
-	@GetMapping(value ="/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		logger.info("PORT = " + environment.getProperty("local.server.port"));
+
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
